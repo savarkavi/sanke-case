488cf883f0f755b0be892343a74808a5172b5863
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-06-15 15:10:36 +0530

    updated

--- actions/createUser.ts
+++ actions/createUser.ts
@@ -4,8 +4,10 @@ import { db } from "@/db";
 
 export const createUser = async ({ email }: { email: string | undefined }) => {
   console.log("user saved in DB");
+  console.log(email);
 
   if (!email) return;
+  console.log("okay");
 
   try {
     await db.user.create({
